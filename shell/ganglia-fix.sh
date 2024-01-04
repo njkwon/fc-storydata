063618615ad841a8e2944cf3d163f278f00de0a9
NakJoo Kwon <njkwon@gmail.com>
2024-01-04 15:36:47 +0900

    Delete shell/ganglia-fix.sh

--- shell/ganglia-fix.sh
+++
@@ -1,5 +0,0 @@
-if [ -f "/etc/httpd/conf.d/ganglia.conf" ]; then
-    echo "Setting up ganglia on master node"
-    sudo sed -i 's/Order deny,allow/#Order deny,allow/g' /etc/httpd/conf.d/ganglia.conf
-    sudo service httpd reload
-fi
