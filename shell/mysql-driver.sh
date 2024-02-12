9ec5b1d9302f3d0cbfdb8439bf6bcf150dfea147
NakJoo Kwon <njkwon@gmail.com>
2024-02-12 18:13:01 +0900

    Update mysql-driver.sh

--- shell/mysql-driver.sh
+++ shell/mysql-driver.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-wget https://repo1.maven.org/maven2/mysql/mysql-connector-java/5.1.40/mysql-connector-java-5.1.40.jar
-sudo mv ./mysql-connector-java-5.1.40.jar /usr/lib/spark/jars
+
+wget https://repo1.maven.org/maven2/mysql/mysql-connector-java/8.0.28/mysql-connector-java-8.0.28.jar
+
+sudo mv mysql-connector-java-8.0.28.jar /usr/lib/spark/jars/
